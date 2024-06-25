74f9db8b88c0456016583fb77313d1788d3de90b
bepa <pavel.belov@zuhlke.com>
2024-06-25 13:04:24 +0300

    added custom searchBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fitness_food_app/components/my_app_bar.dart';
+import 'package:flutter_fitness_food_app/components/my_search_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,9 +9,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: MyAppBar(),
+      ),
+      body: Column(
+        children: [
+          MySearchBar(),
+        ],
       ),
     );
   }
